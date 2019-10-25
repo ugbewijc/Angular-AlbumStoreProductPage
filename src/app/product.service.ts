b513e5c3658b0c23277de6e66bc756aaabbb8f9e
Ugbewi John Chioma <ugbewijc@gmail.com>
2019-10-25 11:52:04 +0100

    Added Interfaces to Describe Data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
+import { Album } from './album';
 
 import "rxjs/add/operator/map";
 
@@ -11,8 +13,8 @@ export class ProductService {
   constructor(private _http: Http) { }
 
 
-  getAlbum(id:number){
-    return this._http.get(this._albumUrl).map(response => response.json());
+  getAlbum(id:number):Observable<Album> {
+    return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
 
 }
